e37d395795cae3a15dc767455226a908cd1718ef
miyazi777 <m71203@gmail.com>
2017-05-10 22:52:02 +0900

    新しいタスクを追加

--- src/main.js
+++ src/main.js
@@ -1,29 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var Hello = React.createClass({
+var Todo = React.createClass({
 
   getInitialState: function() {
     return {
 			tasks: this.props.tasks,
+			newTask: ''
     };
   },
 
+	_textChange(e) {
+		this.setState({newTask: e.target.value});
+	},
+
+	// タスクの追加処理
+	_addTask: function() {
+		tasks = this.state.tasks
+    tasks.push(this.state.newTask);
+		this.setState({
+			newTask: '',
+			tasks: tasks,
+		});
+	},
+
+	// 表示処理
 	render: function() {
 		return (
-      <div>{
-        this.state.tasks.map(function(task) {
-					return <div>{task}</div>;
-				})
-			}</div>
+      <div>
+			  <div>
+			    {
+            this.state.tasks.map(function(task) {
+			    		return <div>{task}</div>;
+			    	})
+			    }
+			  </div>
+			  <div>
+			    <input type="text" onChange={this._textChange} value={this.state.newTask}/>
+				  <button type="button" onClick={this._addTask}>add task</button>
+			  </div>
+			</div>
 		);
 	},
 
 });
 
+
 var tasks = ['task1', 'task2', 'task3'];
 
 ReactDOM.render(
-    <Hello tasks={tasks} />,
+    <Todo tasks={tasks} />,
     document.getElementById('app')
 );
